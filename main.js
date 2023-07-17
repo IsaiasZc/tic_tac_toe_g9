f53bbef8858c8f9d0be5b8cdc1439b53a9050df4
IsaiasZc <coroxerox2@gmail.com>
2023-07-17 10:17:21 -0500

    17/07

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 const casillas = document.querySelectorAll(".casilla");
+let jugador = "X";
+const jugadasGanadoras = [
+  ["1", "2", "3"],
+  ["4", "5", "6"],
+  ["7", "8", "9"],
+  ["1", "4", "7"],
+  ["2", "5", "8"],
+  ["3", "6", "9"],
+  ["1", "5", "9"],
+  ["3", "5", "7"]
+];
+
+const jugadas = {
+  X: [],
+  O: []
+}
+
 // [1,2,3,4,5,6]
 casillas.forEach(addEvent); // por cada elemento en las casillas, ejecuta una funcion
 
@@ -13,6 +30,31 @@ function addEvent(casilla) {
 
 function jugada(event) {
   const val = event.target.getAttribute("data-val");
-  console.log(val);
-  console.log("hola, funciono :D");
+
+  // Si el casillero está lleno, no continues
+  if (event.target.innerHTML) return;
+
+  event.target.innerHTML = jugador;
+
+  guardarJugada(jugador, val);
+
+
+
+  // cambiar turno
+  cambiarJugador();
+}
+
+function cambiarJugador() {
+  // if (jugador == "X") {
+  //   jugador = "O"
+  // } else {
+  //   jugador = "X"
+  // }
+
+  jugador = jugador === "X" ? "O" : "X";
+}
+
+function guardarJugada(jugador, val) {
+  jugadas[jugador].push(val)
+  console.log(jugadas)
 }
